52d29e6180b379e6904c47aeb42a68487c66e431
Chenul Dulmika Thenuwara <chenuldulmikathenuwara@gmail.com>
2024-11-17 01:05:29 +0530

    Chapter tree

---
+++ build/js/main.js
@@ -0,0 +1,14 @@
+const initapp =() => {
+  const hamburgerBtn = document.getElementById('hamburger-button')
+  const mobileMenu = document.getElementById('mobile-menu')
+
+  const toggelMenu = () =>{
+    mobileMenu.classList.toggle('hidden')
+    mobileMenu.classList.toggle('flex')
+    hamburgerBtn.classList.toggle('toggle-btn')
+  }
+  hamburgerBtn.addEventListener('click', toggelMenu)
+  mobileMenu.addEventListener('click', toggelMenu)
+}
+
+document.addEventListener('DOMContentLoaded', initapp)
